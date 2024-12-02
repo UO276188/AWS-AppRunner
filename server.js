f92b8aa7e9f658f6ebd777972228da980f3b56ba
uo276188 <uo276188@uniovi.es>
2024-12-02 14:52:06 +0100

    Adding Name and UO

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World! My name is Sara, UO276188');
 });
 
 app.listen(port, () => {
